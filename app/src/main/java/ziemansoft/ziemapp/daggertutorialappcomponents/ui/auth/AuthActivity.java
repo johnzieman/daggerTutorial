562dd61a27c839730da0942ce50a29fa9113be26
John Zieman <j.g.zokirov@gmail.com>
2020-11-22 22:02:59 +0300

    Livedata components

--- app/src/main/java/ziemansoft/ziemapp/daggertutorialappcomponents/ui/auth/AuthActivity.java
+++ app/src/main/java/ziemansoft/ziemapp/daggertutorialappcomponents/ui/auth/AuthActivity.java
@@ -1,11 +1,14 @@
 package ziemansoft.ziemapp.daggertutorialappcomponents.ui.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 
@@ -16,10 +19,16 @@ import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 import ziemansoft.ziemapp.daggertutorialappcomponents.R;
+import ziemansoft.ziemapp.daggertutorialappcomponents.pojo.User;
 import ziemansoft.ziemapp.daggertutorialappcomponents.viewmodels.ViewModelProvidersFactory;
 
 public class AuthActivity extends DaggerAppCompatActivity{
     private ImageView imageView;
+    private EditText editText;
+    private final String TAG = "AuthActivityViewModel";
+
+
+    private AuthViewModel authViewModel;
 
     @Inject
     Picasso picasso;
@@ -33,10 +42,28 @@ public class AuthActivity extends DaggerAppCompatActivity{
         setContentView(R.layout.activity_auth);
         init();
         picasso.load(R.drawable.icon_user).into(imageView);
-        AuthViewModel authViewModel = new ViewModelProvider(this, viewModelProvidersFactory).get(AuthViewModel.class);
+        editText = findViewById(R.id.editTextTextMultiLine);
+        authViewModel = new ViewModelProvider(this, viewModelProvidersFactory).get(AuthViewModel.class);
     }
 
     private void init(){
         imageView = findViewById(R.id.imageViewLogo);
+    }
+
+    public void getEmailValue(View view) {
+        if(editText.getText().toString().isEmpty()){
+            return;
+        }
+        authViewModel.authenticateWithId(Integer.parseInt(editText.getText().toString()));
+        subscribeObservers();
+    }
+
+    private void subscribeObservers(){
+        authViewModel.observeUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                Log.d(TAG, "On changed: "+ user.getEmail());
+            }
+        });
     }
 }
--- app/src/main/java/ziemansoft/ziemapp/daggertutorialappcomponents/ui/auth/AuthViewModel.java
+++ app/src/main/java/ziemansoft/ziemapp/daggertutorialappcomponents/ui/auth/AuthViewModel.java
@@ -2,11 +2,14 @@ package ziemansoft.ziemapp.daggertutorialappcomponents.ui.auth;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
 
-import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
@@ -14,38 +17,33 @@ import ziemansoft.ziemapp.daggertutorialappcomponents.network.AuthApi;
 import ziemansoft.ziemapp.daggertutorialappcomponents.pojo.User;
 
 public class AuthViewModel extends ViewModel {
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
     private final String TAG = "AuthActivityViewModel";
 
     private final AuthApi authApi;
 
     @Inject
-    public AuthViewModel(AuthApi authApi){
+    public AuthViewModel(AuthApi authApi) {
         this.authApi = authApi;
         Log.d(TAG, "EveryThing is working");
-
-        authApi.getUser(6)
-                .toObservable()
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Observer<User>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(@NonNull User user) {
-                        Log.d(TAG, user.getEmail());
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Log.d(TAG, "onError " + e);
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
     }
+
+    public void authenticateWithId(int id) {
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                authApi.getUser(id)
+                .subscribeOn(Schedulers.io())
+        );
+
+        authUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                authUser.setValue(user);
+                authUser.removeSource(source);
+            }
+        });
+    }
+    public LiveData<User> observeUser() {
+        return authUser;
+    }
+
 }
